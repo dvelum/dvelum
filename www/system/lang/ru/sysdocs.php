69afe09701b9ba2af420fd0d22c36cdc80585ccf
Kirill <kirill.a.egorov@gmail.com>
2015-01-17 15:37:29 +0300

    Docs localization removed

--- www/system/lang/ru/sysdocs.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  'show' => 'Показать',
-  'properties' => 'Свойства',
-  'methods' => 'Методы',
-  'optional' => 'Необязательный',
-  'dvelum_api_doc' => 'DVelum API  Документация',
-); 
