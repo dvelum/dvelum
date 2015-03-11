fe55ff8d2417ad3eac4c944564bf8d1cd3130dbe
Kirill <kirill.a.egorov@gmail.com>
2015-03-11 22:55:56 +0300

    Filestorage unique filename

--- system/library/Filestorage/Simple.php
+++ system/library/Filestorage/Simple.php
@@ -39,26 +39,26 @@ class Filestorage_Simple extends Filestorage_Abstract
     	$fileList = Request::files();
     	$files = array();
 
-    	foreach ($fileList as $item => $cfg)
-    	{
-    		if(is_array($cfg) && !isset($cfg['name']))
-    		{
-    			foreach ($cfg as $item){
-    				$item['old_name'] = $item['name'];
-    				if($this->_config->get('rename')){
-    					$item['name'] = time().File::getExt($item['name']);
-    				}
-    				$files[]= $item;
-    			}
+        foreach ($fileList as $item => $cfg)
+        {
+            if(is_array($cfg) && !isset($cfg['name']))
+            {
+                foreach ($cfg as $item){
+                    $item['old_name'] = $item['name'];
+                    if($this->_config->get('rename')){
+                        $item['name'] = time() . uniqid('-') .File::getExt($item['name']);
+                    }
+                    $files[]= $item;
+                }
 
-    		}else{
-    			$cfg['old_name'] = $cfg['name'];
-    			if($this->_config->get('rename')){
-    				$cfg['name'] = time().File::getExt($cfg['name']);
-    			}
-    			$files[]= $cfg;
-    		}
-    	}
+            }else{
+                $cfg['old_name'] = $cfg['name'];
+                if($this->_config->get('rename')){
+                    $cfg['name'] = time() . uniqid('-') .File::getExt($cfg['name']);
+                }
+                $files[]= $cfg;
+            }
+        }
 
     	if(empty($files))
             return array();
