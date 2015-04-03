125c620ae920f259872fd9fe0f4243326bbefdc4
Kirill <kirill.a.egorov@gmail.com>
2015-04-03 17:53:14 +0300

    Install documentation notice

--- www/install/lang/en.php
+++ www/install/lang/en.php
@@ -25,7 +25,7 @@ array(
 
 	'ADM_EMAIL' => 'Admin email',
 
-	'INSTALL_DOCS' => 'Install documentation DB',
+	'INSTALL_DOCS' => 'Install documentation DB (experimental, Linux only)',
 	'INSTALL_DOCS_ERROR' => 'Unable to install documentation DB',
 
 	'FAILURE_DB_CHECK' => 'Error connecting to the database. Please check your settings and try again',
