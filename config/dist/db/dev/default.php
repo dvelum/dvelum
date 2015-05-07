bae1d34fcb0788b204ee56a542136825b4ab2e83
Kirill <kirill.a.egorov@gmail.com>
2015-05-07 10:17:25 +0300

    Default prefix

--- config/dist/db/dev/default.php
+++ config/dist/db/dev/default.php
@@ -5,7 +5,7 @@ return array(
 	'dbname'   => 'dvelum_5',
 	'host'     => 'localhost',
 	'charset'  => 'UTF8',
-	'prefix'   => 'sb_',
+	'prefix'   => 'dv_',
 	'adapter'  => 'Mysqli',
 	'adapterNamespace' => 'Db_Adapter'
 );
--- config/dist/db/dev/error.php
+++ config/dist/db/dev/error.php
@@ -5,7 +5,7 @@ return array(
 	'dbname'   => 'dvelum_test',
 	'host'     => 'localhost',
 	'charset'  => 'UTF8',
-	'prefix'   => 'sb_',
+	'prefix'   => 'dv_',
 	'adapter'  => 'Mysqli',
 	'adapterNamespace' => 'Db_Adapter'
 );
