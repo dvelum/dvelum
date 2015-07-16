f915fe6ee46930de99e8a5c6ba9f3c78e43ba774
Kirill <kirill.a.egorov@gmail.com>
2015-07-16 09:52:44 +0300

    ACL icons fix

--- www/js/app/system/crud/acl.js
+++ www/js/app/system/crud/acl.js
@@ -128,9 +128,9 @@ Ext.define('app.crud.acl.Permissions',{
 					renderer:function(value, metaData, record, rowIndex, colIndex, store){
 						var allChecked = this.checkPermissionsCol(record);
 						if(allChecked)
-							return '<img src="'+app.wwwRoot+'js/lib/extjs4/resources/themes/images/default/menu/checked.gif">';
+							return '<img src="'+app.wwwRoot+'i/system/checked.gif">';
 						else
-							return '<img src="'+app.wwwRoot+'js/lib/extjs4/resources/themes/images/default/menu/unchecked.gif">';
+							return '<img src="'+app.wwwRoot+'i/system/unchecked.gif">';
 					}
 				},{
 					text:appLang.VIEW,
