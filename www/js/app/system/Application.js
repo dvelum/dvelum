de845d06e50f1aacd77a8fd7c0efb2d180e336d9
Kirill <kirill.a.egorov@gmail.com>
2015-07-28 01:35:43 +0300

    Admin UI. Collapsible menu

--- www/js/app/system/Application.js
+++ www/js/app/system/Application.js
@@ -1,67 +1,78 @@
 app.application = false;
 app.content =  Ext.create('Ext.Panel',{
-	region: 'center',
 	frame:false,
 	border:false,
 	layout:'fit',
-	margins: '0 5 0 0',
+	//margins: '0 5 0 0',
 	scrollable:false,
 	items:[],
-	collapsible:false
+	collapsible:false,
+	flex : 1
 });
-
-app.cookieProvider = new Ext.state.CookieProvider({
-	expires: new Date(new Date().getTime()+(1000*60*60*24)) //1 day
-});
-
 app.header = Ext.create('Ext.Panel',{
-	region:'north',
 	contentEl:'header',
 	bodyCls:'formBody',
 	cls: 'adminHeader',
 	height: 30
 });
 
+Ext.state.Manager.setProvider(new Ext.state.LocalStorageProvider());
+app.cookieProvider = new Ext.state.CookieProvider({
+	expires: new Date(new Date().getTime()+(1000*60*60*24)) //1 day
+});
+
 Ext.application({
 	name: 'DVelum',
+	menuStateKey:'_mState',
 	launch: function() {
 		app.application = this;
-
-		this.buildMenu(app.menuData);
-
+		app.menu = this.buildMenu(app.menuData);
+		app.content.addDocked(app.menu);
 		app.viewport = Ext.create('Ext.container.Viewport', {
-			layout: 'border',
-			items:[app.header, app.content , app.menu]
+			cls:'formBody',
+			layout: {
+				type: 'vbox',
+				pack: 'start',
+				align: 'stretch'
+			},
+			items:[app.header, app.content]
 		});
+		app.viewport.updateLayout();
 	},
 	buildMenu:function(menuData){
-
 		var menuButtons = [];
-
 		Ext.each(menuData,function(item){
 
 			if(!developmentMode && item.dev){
 				return;
 			}
-
 			menuButtons.push({
+				xtype:'button',
 				tooltip:item.title,
 				href:item.url,
 				hrefTarget:'_self',
-				text:'<img src="'+item.icon+'" width="30" height="30"/>',
+				//text:'<img src="'+item.icon+'" width="32" height="32"/> ',
+				text:'<img src="'+item.icon+'" width="14" height="14"/> ' + item.title,
 				textAlign:'left'
 			});
 		});
-
-		app.menu = Ext.create('Ext.Panel',{
-			region: 'west',
-			scrollable:true,
-			split: false,
-			lbar: {
-				items:menuButtons,
-				enableOverflow:true,
-				xtype:'toolbar'
-			}
+		return Ext.create('Ext.Panel', {
+			dock:'left',
+			animCollapse:false,
+			frame:false,
+			border:false,
+			stateEvents: ['expand', 'collapse'],
+			stateful: true,
+			stateId:this.menuStateKey,
+			title:appLang.MENU,
+			collapseDirection:'left',
+			collapsible:true,
+			lbar:menuButtons,
+			enableOverflow:true
 		});
 	}
 });
+/**
+ * 			    collapsed:false,
+
+ */
