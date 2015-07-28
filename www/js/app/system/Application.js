050eb6a6f63c5e084c1a54ef9134a723b1eb19cc
Kirill <kirill.a.egorov@gmail.com>
2015-07-28 11:23:49 +0300

    Collapsible admin menu

--- www/js/app/system/Application.js
+++ www/js/app/system/Application.js
@@ -21,12 +21,109 @@ app.cookieProvider = new Ext.state.CookieProvider({
 	expires: new Date(new Date().getTime()+(1000*60*60*24)) //1 day
 });
 
+Ext.define('app.menuPanel',{
+	extend:'Ext.Panel',
+	isVertical:true,
+	menuData:false,
+	frame:false,
+	border:false,
+	devMode:false,
+	menuCollapsed:false,
+	initComponent:function(){
+		this.isVertical ? this.dock='left' : this.dock='top';
+		this.toolbarContainer = Ext.create('Ext.toolbar.Toolbar',{
+			enableOverflow:true,
+			dock:this.dock
+		});
+		this.dockedItems = [this.toolbarContainer];
+		this.callParent();
+		if(this.stateful){
+			this.on('staterestore', function(menu,state,eOpts){
+				this.menuCollapsed = state.menuCollapsed;
+				this.showButtons();
+			},this);
+		}
+		this.showButtons();
+	},
+	showButtons:function(){
+		this.toolbarContainer.removeAll();
+		var menuButtons = [];
+
+		if(this.menuCollapsed){
+			menuButtons.push({
+				iconAlign:'right',
+				textAlign:'right',
+				width:22,
+				maxWidth:22,
+				icon:  app.wwwRoot + 'i/system/right-btn.gif',
+				scope:this,
+				handler:this.expandMenu
+			});
+		}else{
+			menuButtons.push(
+				{
+					iconAlign:'right',
+					textAlign:'right',
+					width:22,
+					maxWidth:22,
+					icon:  app.wwwRoot + 'i/system/left-btn.gif',
+					scope:this,
+					handler:this.collapseMenu
+				});
+		}
+
+		Ext.each(this.menuData,function(item){
+			if(!this.devMode && item.dev){
+				return;
+			}
+			if(this.menuCollapsed){
+				menuButtons.push({
+					tooltip:item.title,
+					href:item.url,
+					hrefTarget:'_self',
+					text:'<img src="'+item.icon+'" width="32" height="32"/> ',
+					textAlign:'left'
+				});
+			}else{
+				menuButtons.push({
+					xtype:'button',
+					tooltip:item.title,
+					href:item.url,
+					hrefTarget:'_self',
+					text:'<img src="'+item.icon+'" width="14" height="14"/> ' + item.title,
+					textAlign:'left'
+				});
+			}
+		},this);
+		this.toolbarContainer.add(menuButtons);
+	},
+	collapseMenu:function(){
+		this.menuCollapsed = true;
+		this.showButtons();
+		this.fireEvent('menuCollapsed');
+	},
+	expandMenu:function(){
+		this.menuCollapsed = false;
+		this.showButtons();
+		this.fireEvent('menuExpanded');
+	},
+	getState:function(){
+		return {menuCollapsed:this.menuCollapsed};
+	}
+});
+
 Ext.application({
 	name: 'DVelum',
-	menuStateKey:'_mState',
 	launch: function() {
 		app.application = this;
-		app.menu = this.buildMenu(app.menuData);
+		app.menu = Ext.create('app.menuPanel',{
+			menuData:app.menuData,
+			isVertical:true,
+			devMode:developmentMode,
+			stateEvents: ['menuCollapsed', 'menuExpanded'],
+			stateful: true,
+			stateId:'_appMenuState'
+		});
 		app.content.addDocked(app.menu);
 		app.viewport = Ext.create('Ext.container.Viewport', {
 			cls:'formBody',
@@ -37,42 +134,5 @@ Ext.application({
 			},
 			items:[app.header, app.content]
 		});
-		app.viewport.updateLayout();
-	},
-	buildMenu:function(menuData){
-		var menuButtons = [];
-		Ext.each(menuData,function(item){
-
-			if(!developmentMode && item.dev){
-				return;
-			}
-			menuButtons.push({
-				xtype:'button',
-				tooltip:item.title,
-				href:item.url,
-				hrefTarget:'_self',
-				//text:'<img src="'+item.icon+'" width="32" height="32"/> ',
-				text:'<img src="'+item.icon+'" width="14" height="14"/> ' + item.title,
-				textAlign:'left'
-			});
-		});
-		return Ext.create('Ext.Panel', {
-			dock:'left',
-			animCollapse:false,
-			frame:false,
-			border:false,
-			stateEvents: ['expand', 'collapse'],
-			stateful: true,
-			stateId:this.menuStateKey,
-			title:appLang.MENU,
-			collapseDirection:'left',
-			collapsible:true,
-			lbar:menuButtons,
-			enableOverflow:true
-		});
 	}
 });
-/**
- * 			    collapsed:false,
-
- */
