2817f4a1b30284d2fc909c7f9a69dd65d255486a
Kirill <kirill.a.egorov@gmail.com>
2015-10-17 10:34:08 +0300

    Maps update

--- application/configs/dist/umlMap.php
+++ application/configs/dist/umlMap.php
@@ -1,6 +1,4 @@
 <?php return array (
-  'mapWidth' => 2000,
-  'mapHeight' => 2000,
   'items' => 
   array (
     'user' => 
@@ -23,110 +21,110 @@
       'x' => 1010,
       'y' => 187,
     ),
-    'bgtask' => 
+    'menu' => 
     array (
-      'x' => 778,
-      'y' => 548,
+      'x' => 241,
+      'y' => 471,
     ),
     'mediacategory' => 
     array (
       'x' => 584,
       'y' => 817,
     ),
-    'menu' => 
+    'bgtask' => 
     array (
-      'x' => 241,
-      'y' => 471,
-    ),
-    'sysdocs_class_method' => 
-    array (
-      'x' => 1716,
-      'y' => 255,
+      'x' => 778,
+      'y' => 548,
     ),
     'sysdocs_file' => 
     array (
       'x' => 1270,
       'y' => 281,
     ),
-    'blocks' => 
-    array (
-      'x' => 452,
-      'y' => 336,
-    ),
     'medialib' => 
     array (
       'x' => 591,
       'y' => 577,
     ),
-    'menu_item' => 
+    'blocks' => 
     array (
-      'x' => 49,
-      'y' => 589,
+      'x' => 452,
+      'y' => 336,
     ),
-    'sysdocs_class_property' => 
+    'sysdocs_class_method' => 
     array (
-      'x' => 1476,
-      'y' => 512,
+      'x' => 1716,
+      'y' => 255,
     ),
     'links' => 
     array (
       'x' => 371,
       'y' => 745,
     ),
-    'online' => 
+    'error_log' => 
     array (
-      'x' => 269,
-      'y' => 199,
+      'x' => 1014,
+      'y' => 484,
     ),
-    'acl_simple' => 
+    'sysdocs_class_property' => 
     array (
-      'x' => 1008,
-      'y' => 316,
-    ),
-    'bgtask_signal' => 
-    array (
-      'x' => 779,
-      'y' => 448,
-    ),
-    'blockmapping' => 
-    array (
-      'x' => 243,
-      'y' => 340,
-    ),
-    'vc' => 
-    array (
-      'x' => 823,
-      'y' => 319,
-    ),
-    'historylog' => 
-    array (
-      'x' => 250,
-      'y' => 13,
-    ),
-    'sysdocs_localization' => 
-    array (
-      'x' => 1485,
-      'y' => 100,
+      'x' => 1476,
+      'y' => 512,
     ),
     'permissions' => 
     array (
       'x' => 1004,
       'y' => 6,
     ),
-    'sysdocs_class_method_param' => 
+    'sysdocs_localization' => 
     array (
-      'x' => 1709,
-      'y' => 513,
+      'x' => 1485,
+      'y' => 100,
     ),
-    'apikeys' => 
+    'vc' => 
     array (
-      'x' => 231,
-      'y' => 776,
+      'x' => 823,
+      'y' => 319,
     ),
     'filestorage' => 
     array (
       'x' => 697,
       'y' => 321,
+    ),
+    'sysdocs_class_method_param' => 
+    array (
+      'x' => 1709,
+      'y' => 513,
+    ),
+    'bgtask_signal' => 
+    array (
+      'x' => 779,
+      'y' => 448,
+    ),
+    'historylog' => 
+    array (
+      'x' => 250,
+      'y' => 13,
+    ),
+    'user_auth' => 
+    array (
+      'x' => 329,
+      'y' => 217,
+    ),
+    'blockmapping' => 
+    array (
+      'x' => 243,
+      'y' => 340,
+    ),
+    'acl_simple' => 
+    array (
+      'x' => 1008,
+      'y' => 316,
+    ),
+    'menu_item' => 
+    array (
+      'x' => 49,
+      'y' => 589,
     ),
   ),
 ); 
