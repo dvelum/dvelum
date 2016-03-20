e107e7af7aefd7dcd2387b024ca075fc42472d89
Kirill <kirill.a.egorov@gmail.com>
2016-03-20 09:33:01 +0300

    cleanup JS  includes for backend

--- application/configs/dist/js_inc_backend.php
+++ application/configs/dist/js_inc_backend.php
@@ -9,21 +9,8 @@ return array(
         '/js/app/system/RevisionPanel.js' => array('order'=>0 ,'minified'=>false),
     	'/js/app/system/EditWindow.js' => array('order'=>0 ,'minified'=>false),
         '/js/app/system/ContentWindow.js' => array('order'=>0 ,'minified'=>false),
-    	//'/js/lib/extjs4/ux/RowExpander.js' => array('order'=>0 ,'minified'=>false),
-     /*   '/js/lib/extjs4/ux/grid/menu/ListMenu.js' => array('order'=>5 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/menu/RangeMenu.js' => array('order'=>5 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/FiltersFeature.js' => array('order'=>4 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/Filter.js' => array('order'=>5 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/BooleanFilter.js' => array('order'=>6 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/DateFilter.js' => array('order'=>6 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/DateTimeFilter.js' => array('order'=>7 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/ListFilter.js' => array('order'=>7 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/NumericFilter.js' => array('order'=>7 ,'minified'=>false),
-        '/js/lib/extjs4/ux/grid/filter/StringFilter.js' => array('order'=>7 ,'minified'=>false)
-    */
     ),
     'css'=>array(
-   //     '/js/lib/extjs4/ux/grid/css/GridFilters.css' => array('order'=>1 , 'minified'=>false),
-   //     '/js/lib/extjs4/ux/grid/css/RangeMenu.css' => array('order'=>1 , 'minified'=>false)
+
     )
 );
