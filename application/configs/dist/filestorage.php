7f50cd8ed6267a268daa3cdd659a10539785ebaa
Kirill <kirill.a.egorov@gmail.com>
2016-05-04 00:54:33 +0300

    Filestorage config fix

--- application/configs/dist/filestorage.php
+++ application/configs/dist/filestorage.php
@@ -1,80 +1,79 @@
 <?php
 return array(
-	'development' =>
-		array(
-			'adapter'=>'Orm',
-			'filepath' => __DIR__ . '/../../../filestorage',
-			'object'=>'filestorage',
-			'check_orm_structure'=> false,
-			'user_id'=>0, //default user ID
-			'upload_prefix'=>'',
-			'rename'=>true,
-			'uploader' => 'Upload',
-			'uploader_config' => array(
-				'file' => array(
-					'title' => 'Files' ,
-					'extensions' => array(
-							'.doc' ,
-							'.docx' ,
-							'.pdf' ,
-							'.rar' ,
-							'.zip' ,
-							'.odt' ,
-							'.txt' ,
-							'.xls' ,
-							'.xlsx' ,
-							'.ods',
-							'.csv',
-							'.gif' ,
-							'.png' ,
-							'.jpg' ,
-							'.jpeg',
-							'.eml'
-					)
+		'development' =>
+				array(
+						'adapter'=>'Orm',
+						'filepath' => __DIR__ . '/../../../filestorage',
+						'object'=>'filestorage',
+						'check_orm_structure'=> false,
+						'user_id'=>0, //default user ID
+						'upload_prefix'=>'',
+						'rename'=>true,
+						'uploader' => 'Upload',
+						'uploader_config' => array(
+								'file' => array(
+										'title' => 'Files' ,
+										'extensions' => array(
+												'.doc' ,
+												'.docx' ,
+												'.pdf' ,
+												'.rar' ,
+												'.zip' ,
+												'.odt' ,
+												'.txt' ,
+												'.xls' ,
+												'.xlsx' ,
+												'.ods',
+												'.csv',
+												'.gif' ,
+												'.png' ,
+												'.jpg' ,
+												'.jpeg',
+												'.eml'
+										)
+								)
+						),
+						'download'=>array(
+								'type'=> 'native', // native / apache / nginx
+								'redirect_path' => ''
+						)
+				),
+		'production' => array(
+
+				'adapter'=>'Orm',
+				'filepath' => __DIR__ . '/../../../filestorage',
+				'object'=>'filestorage',
+				'check_orm_structure'=> false,
+				'user_id'=>0, //default user ID
+				'upload_prefix'=>'',
+				'rename'=>true,
+				'uploader' => 'Upload',
+				'uploader_config' => array(
+						'file' => array(
+								'title' => 'Files' ,
+								'extensions' => array(
+										'.doc' ,
+										'.docx' ,
+										'.pdf' ,
+										'.rar' ,
+										'.zip' ,
+										'.odt' ,
+										'.txt' ,
+										'.xls' ,
+										'.xlsx' ,
+										'.ods',
+										'.csv',
+										'.gif' ,
+										'.png' ,
+										'.jpg' ,
+										'.jpeg',
+										'.eml'
+								)
+						)
+				),
+				'download'=>array(
+						'type'=> 'native', // native / apache / nginx
+						'redirect_path' => ''
 				)
-			),
-			'download'=>array(
-				'type'=> 'native', // native / apache / nginx
-				'redirect_path' => ''
-			)
 		),
-	'production' => array(
-		array(
-			'adapter'=>'Orm',
-			'filepath' => __DIR__ . '/../../../filestorage',
-			'object'=>'filestorage',
-			'check_orm_structure'=> false,
-			'user_id'=>0, //default user ID
-			'upload_prefix'=>'',
-			'rename'=>true,
-			'uploader' => 'Upload',
-			'uploader_config' => array(
-				'file' => array(
-					'title' => 'Files' ,
-					'extensions' => array(
-						'.doc' ,
-						'.docx' ,
-						'.pdf' ,
-						'.rar' ,
-						'.zip' ,
-						'.odt' ,
-						'.txt' ,
-						'.xls' ,
-						'.xlsx' ,
-						'.ods',
-						'.csv',
-						'.gif' ,
-						'.png' ,
-						'.jpg' ,
-						'.jpeg',
-						'.eml'
-					)
-				)
-			),
-			'download'=>array(
-				'type'=> 'native', // native / apache / nginx
-				'redirect_path' => ''
-			)
-		),
-	)
 );
