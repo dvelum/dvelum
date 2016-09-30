bcbe501074d97409713dd97e3040e53d2015ad48
hartois <hartois@gmail.com>
2016-09-30 23:18:49 +0300

    Fix Frontend_Backoffice controller

--- dvelum/app/Frontend/Controller/Backoffice.php
+++ dvelum/app/Frontend/Controller/Backoffice.php
@@ -18,28 +18,28 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
      * Module id assigned to controller;
      * Is to be defined in child class
      * Is used for controlling access permissions
-     * 
+     *
      * @var string
      */
     protected $_module;
 
     /**
      * Link to Config object of the backend application
-     * 
+     *
      * @var Config_Abstract
      */
     protected $_configBackoffice;
 
     /**
      * Link to Config object of the connected JS files
-     * 
+     *
      * @var Config_Abstract
      */
     protected $_configJs;
 
     /**
      * Link to User object (current user)
-     * 
+     *
      * @var User
      */
     protected $_user;
@@ -57,11 +57,11 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
         $this->_module = $this->getModule();
         $this->_lang = Lang::lang();
         $this->_configMain = Registry::get('main' , 'config');
-        
+
         $cacheManager = new Cache_Manager();
         $this->_configBackoffice = Config::storage()->get('backend.php');
         $this->_cache = $cacheManager->get('data');
-        
+
         if(Request::get('logout' , 'boolean' , false)){
             User::getInstance()->logout();
             session_destroy();
@@ -69,10 +69,10 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
                 Response::redirect(Request::url(array('index')));
         }
         $this->checkAuth();
-        
+
         if($this->_configBackoffice->get('use_csrf_token')){
-          $csrf = new Security_Csrf();
-          $this->_page->csrfToken = $csrf->createToken();
+            $csrf = new Security_Csrf();
+            $this->_page->csrfToken = $csrf->createToken();
         }
     }
 
@@ -84,18 +84,18 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
         $media = Model::factory('Medialib');
         $media->includeScripts();
         $cfg = Config::storage()->get('js_inc_backend.php');
-        
+
         $this->_resource->addJs('/js/lib/jquery.js',0 , true , 'head');
 
-        $this->_resource->addJs('/js/lib/extjs4/ext-all-debug.js' , 0 , true , 'head');
+        $this->_resource->addJs('/js/lib/ext6/build/ext-all.js' , 0 , true , 'head');
         $this->_resource->addJs('/js/lang/'.$this->_configMain['language'].'.js', 1 , true , 'head');
-        $this->_resource->addJs('/js/lib/extjs4/locale/ext-lang-'.$this->_configMain['language'].'.js', 2 , true , 'head');
+        $this->_resource->addJs('/js/lib/ext6/build/locale/locale-'.$this->_configMain['language'].'.js', 2 , true , 'head');
         $this->_resource->addJs('/js/app/frontend/application.js', 3 , false ,  'head');
         $this->_resource->addJs('/js/app/system/common.js', 3 , false ,  'head');
 
-        $this->_resource->addJs('/js/lib/extjs4/ext-theme-gray.js' , 2);
-        $this->_resource->addCss('/js/lib/extjs4/resources/css/ext-all-gray.css');
-        $this->_resource->addCss('/css/system/default/style.css');
+        $this->_resource->addJs('/js/lib/ext6/build/theme-gray/theme-gray.js' , 2);
+        $this->_resource->addCss('/js/lib/ext6/build/theme-gray/resources/theme-gray-all.css');
+        $this->_resource->addCss('/css/system/gray/style.css');
 
         $this->_resource->addInlineJs('
            var developmentMode = '.intval($this->_configMain->get('development')).';
@@ -106,18 +106,18 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
 
         if($cfg->getCount())
         {
-           $js = $cfg->get('js');
-           if(!empty($js))
-            foreach($js as $file => $config)
-                $this->_resource->addJs($file , $config['order'] , $config['minified']);
-           
-           $css = $cfg->get('css');
-           if(!empty($css))
-              foreach($css as $file => $config)
-                $this->_resource->addCss($file , $config['order']);
+            $js = $cfg->get('js');
+            if(!empty($js))
+                foreach($js as $file => $config)
+                    $this->_resource->addJs($file , $config['order'] , $config['minified']);
+
+            $css = $cfg->get('css');
+            if(!empty($css))
+                foreach($css as $file => $config)
+                    $this->_resource->addCss($file , $config['order']);
         }
     }
-    
+
     /**
      * Check user permissions and authentication
      */
@@ -125,10 +125,10 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
     {
         $user = User::getInstance();
         $uid = false;
-        
+
         if($user->isAuthorized())
             $uid = $user->id;
-        
+
         if(! $uid || ! $user->isAdmin()){
             if(Request::isAjax())
                 Response::jsonError($this->_lang->MSG_AUTHORIZE);
@@ -141,15 +141,15 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
         if($this->_configBackoffice->get('use_csrf_token') && Request::hasPost()){
             $csrf = new Security_Csrf();
             $csrf->setOptions(
-                    array(
-                            'lifetime' => $this->_configBackoffice->get('use_csrf_token_lifetime'),
-                            'cleanupLimit' => $this->_configBackoffice->get('use_csrf_token_garbage_limit')
-                    ));
-            
+                array(
+                    'lifetime' => $this->_configBackoffice->get('use_csrf_token_lifetime'),
+                    'cleanupLimit' => $this->_configBackoffice->get('use_csrf_token_garbage_limit')
+                ));
+
             if(!$csrf->checkHeader() && !$csrf->checkPost())
                 $this->_errorResponse($this->_lang->MSG_NEED_CSRF_TOKEN);
         }
-        
+
         $this->_user = $user;
     }
 
@@ -166,25 +166,25 @@ abstract class Frontend_Controller_Backoffice extends Backend_Controller
 
     public function indexAction()
     {
-      $this->_resource->addInlineJs('
-	     var canEdit = ' . intval($this->_user->canEdit($this->_module)) . ';
-	     var canDelete = ' . intval($this->_user->canDelete($this->_module)) . ';
+        $this->_resource->addInlineJs('
+         var canEdit = ' . intval($this->_user->canEdit($this->_module)) . ';
+         var canDelete = ' . intval($this->_user->canDelete($this->_module)) . ';
          var canPublish = ' . intval($this->_user->canPublish($this->_module)) . ';
-	    ');
-      $this->includeScripts();
+        ');
+        $this->includeScripts();
     }
 
     /**
      * Run Layout project
-     * 
+     *
      * @param string $project - path to project file
      */
     protected function _runDesignerProject($project, $renderTo = false)
-    {    
-      $manager = new Designer_Manager($this->_configMain);
-      $manager->renderProject($project , $renderTo);
+    {
+        $manager = new Designer_Manager($this->_configMain);
+        $manager->renderProject($project , $renderTo);
     }
-    
+
     /**
      * Send JSON error message
      *
