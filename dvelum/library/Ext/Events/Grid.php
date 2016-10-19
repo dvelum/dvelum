35b5e1c921cf0cb796f25a634453ac56a3809339
hartois <hartois@gmail.com>
2016-10-19 10:20:38 +0300

    Ext Grid events: add canceledit, fix beforeedit

--- dvelum/library/Ext/Events/Grid.php
+++ dvelum/library/Ext/Events/Grid.php
@@ -2,24 +2,23 @@
 class Ext_Events_Grid extends Ext_Events_Table
 {
     static protected $_editPluginOptions = array(
-    		'editor'=>' Ext.grid.plugin.Editing',
-    		'e'=>'Object',
-    		'eOpts'=>'Object'
+        'editor'=>' Ext.grid.plugin.Editing',
+        'e'=>'Object',
+        'eOpts'=>'Object'
     );
 
     public $beforeedit;
+    public $canceledit;
     public $edit;
     public $validateedit;
 
     public function _initConfig()
     {
-    	parent::_initConfig();
+        parent::_initConfig();
 
-    	$this->beforeedit = array(
-    		'e'=>'Object',
-    		'eOpts'=>'Object'
-    	);
-    	$this->edit = static::$_editPluginOptions;
-    	$this->validateedit = static::$_editPluginOptions;
+        $this->beforeedit = static::$_editPluginOptions;
+        $this->canceledit = static::$_editPluginOptions;
+        $this->edit = static::$_editPluginOptions;
+        $this->validateedit = static::$_editPluginOptions;
     }
 }
