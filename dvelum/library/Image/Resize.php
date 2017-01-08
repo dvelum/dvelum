1235055df0bb055fcac06058414938dddb95b5b4
k-samuel <kirill.a.egorov@gmail.com>
2017-01-08 15:36:38 +0300

    Image_Resize code cleanup

--- dvelum/library/Image/Resize.php
+++ dvelum/library/Image/Resize.php
@@ -1,24 +1,25 @@
 <?php
-/*
- * DVelum project http://code.google.com/p/phpveil/ , http://dvelum.net
- * Copyright (C) 2011  Kirill A Egorov kirill.a.egorov@gmail.com
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+/**
+ *  DVelum project http://code.google.com/p/dvelum/ , https://github.com/k-samuel/dvelum , http://dvelum.net
+ *  Copyright (C) 2011-2017  Kirill Yegorov
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 /**
- * Image Resizer Component
+ * Image Resize Component
  * @package Image
  * @author Kirill A Egorov kirill.a.egorov@gmail.com
  */
@@ -71,7 +72,7 @@ class Image_Resize
                 $im = imagecreatefrompng($path);
                 break;
             default :
-                trigger_error('Unsupported filetype!' , E_USER_WARNING);
+                trigger_error('Unsupported file type!' , E_USER_WARNING);
                 return false;
                 break;
         }
@@ -84,6 +85,8 @@ class Image_Resize
      * @param integer $width
      * @param integer $height
      * @param string $newImgPath
+     * @param boolean $fit, optional default false
+     * @param boolean $crop, optional default false
      * @return boolean
      */
     static public function resize($imgPath , $width , $height , $newImgPath , $fit = false , $crop = true)
