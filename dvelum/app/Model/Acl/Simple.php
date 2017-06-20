305a9556b2d5b893ad0b50438e12fdc99a1ea8e2
k-samuel <kirill.a.egorov@gmail.com>
2017-06-20 21:54:04 +0300

    ACL Model fix

--- dvelum/app/Model/Acl/Simple.php
+++ dvelum/app/Model/Acl/Simple.php
@@ -5,170 +5,170 @@ use Dvelum\Orm\Model;
 
 class Model_Acl_Simple extends Model
 {
-  static protected $_fields = array('view','create','edit','delete','object','publish');
+    static protected $fields = array('view','create','edit','delete','object','publish');
 
-  /**
-   * Get object permissions for user
-   * @param integer $userId
-   * @param integer $groupId
-   * @throws Exception
-   * @return array
-   */
-  public function getPermissions($userId , $groupId)
-  {
-      if(empty($userId))
-          throw new Exception('Need user id');
+    /**
+     * Get object permissions for user
+     * @param integer $userId
+     * @param integer $groupId
+     * @throws Exception
+     * @return array
+     */
+    public function getPermissions($userId , $groupId)
+    {
+        if(empty($userId))
+            throw new Exception('Need user id');
 
-      $cache = $this->_cache;
+        $cache = $this->cache;
 
-      /*
-       * Check if cache exists
-       */
-      if($cache && $data = $cache->load('object_permissions_' . $userId))
-          return $data;
+        /*
+         * Check if cache exists
+         */
+        if($cache && $data = $cache->load('object_permissions_' . $userId))
+            return $data;
 
-      $data = array();
-      /*
-       * Load permissions for group
-      */
-      if($groupId){
+        $data = array();
+        /*
+         * Load permissions for group
+        */
+        if($groupId){
 
-          $sql = $this->_dbSlave->select()
-          ->from($this->table() , self::$_fields)
-          ->where('`group_id` = '.intval($groupId))
-          ->where('`user_id` IS NULL');
+            $sql = $this->dbSlave->select()
+                ->from($this->table() , self::$fields)
+                ->where('`group_id` = '.intval($groupId))
+                ->where('`user_id` IS NULL');
 
-          $groupRights = $this->_dbSlave->fetchAll($sql);
+            $groupRights = $this->dbSlave->fetchAll($sql);
 
-          if(!empty($groupRights))
-              $data =  Utils::rekey('object', $groupRights);
-      }
-     /*
-      * Load permissions for user
-      */
-      $sql = $this->_dbSlave->select()
-      ->from($this->table() , self::$_fields)
-      ->where('`user_id` = '.intval($userId))
-      ->where('`group_id` IS NULL');
+            if(!empty($groupRights))
+                $data =  Utils::rekey('object', $groupRights);
+        }
+        /*
+         * Load permissions for user
+         */
+        $sql = $this->dbSlave->select()
+            ->from($this->table() , self::$fields)
+            ->where('`user_id` = '.intval($userId))
+            ->where('`group_id` IS NULL');
 
-      $userRights = $this->_dbSlave->fetchAll($sql);
+        $userRights = $this->dbSlave->fetchAll($sql);
 
-      /*
-       * Replace group permissions by permissions redefined for concrete user
-       */
-       if(!empty($userRights))
-          $data = array_merge($data , Utils::rekey('object', $userRights));
+        /*
+         * Replace group permissions by permissions redefined for concrete user
+         */
+        if(!empty($userRights))
+            $data = array_merge($data , Utils::rekey('object', $userRights));
 
-      /*
-       * Cache info
-      */
-      if($cache)
-          $cache->save($data , 'object_permissions' . $userId);
+        /*
+         * Cache info
+        */
+        if($cache)
+            $cache->save($data , 'object_permissions' . $userId);
 
-      return $data;
-  }
+        return $data;
+    }
 
-  /**
-   * Get permissions for user group
-   * Return permissions list indexed by module id
-   * @return array
-   */
-  public function getGroupPermissions($groupId)
-  {
-      $data = array();
-      $cache = $this->_cache;
-      /*
-       * Check if cache exists
-       */
-      if($cache && $data = $cache->load('acl_simple_group_permissions' . $groupId))
-          return $data;
+    /**
+     * Get permissions for user group
+     * Return permissions list indexed by module id
+     * @return array
+     */
+    public function getGroupPermissions($groupId)
+    {
+        $data = [];
+        $cache = $this->cache;
+        /*
+         * Check if cache exists
+         */
+        if($cache && $data = $cache->load('acl_simple_group_permissions' . $groupId))
+            return $data;
 
-      $sql = $this->_dbSlave->select()
-      ->from($this->table() , self::$_fields)
-      ->where('`group_id` = '.intval($groupId))
-      ->where('`user_id` IS NULL');
+        $sql = $this->dbSlave->select()
+            ->from($this->table() , self::$fields)
+            ->where('`group_id` = '.intval($groupId))
+            ->where('`user_id` IS NULL');
 
-      $data = $this->_dbSlave->fetchAll($sql);
+        $data = $this->dbSlave->fetchAll($sql);
 
-      if(!empty($data))
-          $data =  Utils::rekey('object', $data);
+        if(!empty($data))
+            $data =  Utils::rekey('object', $data);
 
-      /*
-       * Cache info
-      */
-      if($cache)
-          $cache->save($data , 'acl_simple_group_permissions' . $groupId);
+        /*
+         * Cache info
+        */
+        if($cache)
+            $cache->save($data , 'acl_simple_group_permissions' . $groupId);
 
-      return $data;
-  }
+        return $data;
+    }
 
- /**
-  * Update group permissions
-  * @param integer $groupId
-  * @param array $data - permissions like array(
-  * array(
-  * 			'object'=>'object',
-  * 			'view'=>true,
-  *             'create'=>false,
-  * 			'edit'=>false,
-  * 			'delete'=>false,
-  * 			'publish'=>false
-  * 	),
-  * 	...
-  * )
-  * @return boolean
-  */
-  public function updateGroupPermissions($groupId , array $data)
-  {
-      $modulesToRemove = Utils::fetchCol('object', $data);
-      if(!empty($modulesToRemove))
-      {
-          try{
-              $this->_db->delete($this->table(),'`object` IN (\''.implode("','", $modulesToRemove).'\') AND `group_id`='.intval($groupId));
-          }catch (Exception $e){
-              $this->logError($e->getMessage());
-              return false;
-          }
-      }
+    /**
+     * Update group permissions
+     * @param integer $groupId
+     * @param array $data - permissions like array(
+     * array(
+     * 			'object'=>'object',
+     * 			'view'=>true,
+     *             'create'=>false,
+     * 			'edit'=>false,
+     * 			'delete'=>false,
+     * 			'publish'=>false
+     * 	),
+     * 	...
+     * )
+     * @return boolean
+     */
+    public function updateGroupPermissions($groupId , array $data)
+    {
+        $modulesToRemove = Utils::fetchCol('object', $data);
+        if(!empty($modulesToRemove))
+        {
+            try{
+                $this->db->delete($this->table(),'`object` IN (\''.implode("','", $modulesToRemove).'\') AND `group_id`='.intval($groupId));
+            }catch (Exception $e){
+                $this->logError($e->getMessage());
+                return false;
+            }
+        }
 
-      $errors = false;
+        $errors = false;
 
-      foreach ($data as $values)
-      {
-          /**
-           * Check if all needed fields are present
-           */
-          $diff = array_diff(self::$_fields, array_keys($values));
+        foreach ($data as $values)
+        {
+            /**
+             * Check if all needed fields are present
+             */
+            $diff = array_diff(self::$fields, array_keys($values));
 
-          if(!empty($diff))
-              continue;
+            if(!empty($diff))
+                continue;
 
-          try{
-              $obj = Orm\Object::factory($this->_name);
-              $obj->setValues(array(
-                  'view'=>(boolean)$values['view'],
-                  'create'=>(boolean)$values['create'],
-                  'edit'=>(boolean)$values['edit'],
-                  'delete'=>(boolean)$values['delete'],
-                  'publish'=>(boolean)$values['publish'],
-                  'object'=>$values['object'],
-                  'group_id'=>$groupId,
-                  'user_id'=>null
-              ));
+            try{
+                $obj = Orm\Object::factory($this->name);
+                $obj->setValues(array(
+                    'view'=>(boolean)$values['view'],
+                    'create'=>(boolean)$values['create'],
+                    'edit'=>(boolean)$values['edit'],
+                    'delete'=>(boolean)$values['delete'],
+                    'publish'=>(boolean)$values['publish'],
+                    'object'=>$values['object'],
+                    'group_id'=>$groupId,
+                    'user_id'=>null
+                ));
 
-              if(!$obj->save()){
-                  $errors = true;
-              }
+                if(!$obj->save()){
+                    $errors = true;
+                }
 
-          }catch (Exception $e){
-             $this->logError($e->getMessage());
-             $errors = true;
-          }
-      }
+            }catch (Exception $e){
+                $this->logError($e->getMessage());
+                $errors = true;
+            }
+        }
 
-      if($errors)
-          return false;
-      else
-      	   return true;
-  }
+        if($errors)
+            return false;
+        else
+            return true;
+    }
 }
