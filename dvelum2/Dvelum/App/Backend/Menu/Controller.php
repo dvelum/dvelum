710596c8d3062802cd3f6c20713f9cf42538c578
hartois <hartois@gmail.com>
2017-12-31 15:54:04 +0300

    Menu Controller 2.x refactoring: changes

--- dvelum2/Dvelum/App/Backend/Menu/Controller.php
+++ dvelum2/Dvelum/App/Backend/Menu/Controller.php
@@ -1,32 +1,62 @@
 <?php
+/**
+ *  DVelum project https://github.com/dvelum/dvelum
+ *  Copyright (C) 2011-2017  Kirill Yegorov
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Dvelum\App\Backend\Menu;
+
+use Dvelum\App\Backend;
 use Dvelum\Orm;
 use Dvelum\Orm\Model;
-use Dvelum\Config;
 
-class Backend_Menu_Controller extends Backend_Controller_Crud
+class Controller extends Backend\Ui\Controller
 {
-	 /**
+
+    public function getModule(): string
+    {
+        return 'Menu';
+    }
+
+    public function getObjectName(): string
+    {
+        return 'Menu';
+    }
+
+    /**
 	  * (non-PHPdoc)
 	  * @see Backend_Controller_Crud::indexAction()
 	  */
 	 public function indexAction()
      {   	
-        $this->_resource->addInlineJs('
-        	var canEdit = '.($this->_user->canEdit($this->_module)).';
-        	var canDelete = '.($this->_user->canDelete($this->_module)).';
-        	var menuItemlinkTypes = '.Dictionary::getInstance('link_type')->__toJs().';
+        $this->resource->addInlineJs('
+        	var canEdit = '.($this->checkCanEdit()).';
+        	var canDelete = '.($this->checkCanDelete()).';
+        	var menuItemlinkTypes = '.\Dictionary::factory('link_type')->__toJs().';
         ');
         
-        $modulesConfig = Config::factory(Config\Factory::File_Array , $this->_configMain->get('backend_modules'));
-       
         Model::factory('Medialib')->includeScripts();
 
-        $this->_resource->addJs('/js/app/system/SearchPanel.js', 0);          
-        $this->_resource->addJs('/js/app/system/HistoryPanel.js', 0);
-        $this->_resource->addJs('/js/app/system/EditWindow.js' , 0);
+        $this->resource->addJs('/js/app/system/SearchPanel.js', 0);          
+        $this->resource->addJs('/js/app/system/HistoryPanel.js', 0);
+        $this->resource->addJs('/js/app/system/EditWindow.js' , 0);
 
-         $this->_resource->addJs('/js/app/system/Menu.js' , 3);
-         $this->_resource->addJs('/js/app/system/crud/menu.js', 4);
+         $this->resource->addJs('/js/app/system/Menu.js' , 3);
+         $this->resource->addJs('/js/app/system/crud/menu.js', 4);
     } 
     
 	/**
@@ -43,7 +73,7 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
             ->fields(['id','code','title'])
             ->fetchAll();
 
-		Response::jsonSuccess($data);
+		$this->response->success($data);
 	}
 	
 	/**
@@ -52,18 +82,18 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
 	 */
     public function loaddataAction()
     { 	
-        $id = Request::post('id', 'integer', false);
+        $id = $this->request->post('id', 'integer', false);
         
         if(!$id)
-            Response::jsonSuccess(array());
+            $this->response->success(array());
               
         try{
             /**
-             * @var Dvelum\Orm\ObjectInterface $obj
+             * @var Orm\ObjectInterface $obj
              */
-            $obj = Orm\Object::factory($this->_objectName, $id);
-        }catch(Exception $e){
-            Response::jsonError($this->_lang->CANT_EXEC);
+            $obj = Orm\Object::factory($this->getObjectName(), $id);
+        }catch(\Exception $e){
+            $this->response->error($this->lang->get('CANT_EXEC'));
         }
 
         $data = $obj->getData();
@@ -83,7 +113,7 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
 
             if($objectField->isObjectLink() || $objectField->isMultiLink()){
                 $linkObject = $obj->getLinkedObject($field);
-                $data[$field] = array_values($this->_collectLinksData($data[$field] ,$linkObject));
+                $data[$field] = array_values($this->collectLinksData($data[$field], $obj, $linkObject));
             }
         }
         $data['id'] = $obj->getId();
@@ -93,7 +123,7 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
         /*
          * Send response
          */
-        Response::jsonSuccess($data);
+        $this->response->success($data);
     }
     
     /**
@@ -103,19 +133,19 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
     {
     	 $pagesModel = Model::factory('Page');
     	 $data = $pagesModel->query()->fields(['id','title'=>'page_title'])->fetchAll();
-    	 Response::jsonSuccess($data);   	 
+    	 $this->response->success($data);   	 
     }
     
  	/**
  	 * (non-PHPdoc)
  	 * @see Backend_Controller_Crud::insertObject()
  	 */
-    public function insertObject(Orm\ObjectInterface $object)
+    public function insertObject(Orm\Object $object)
     {  
          if(!$recId = $object->save())
-             Response::jsonError($this->_lang->CANT_CREATE);
+             $this->response->error($this->lang->get('CANT_CREATE'));
              
-         $linksData = Request::post('data', 'raw', false);
+         $linksData = $this->request->post('data', 'raw', false);
 
          if(strlen($linksData)){
          	$linksData = json_decode($linksData , true);
@@ -126,21 +156,21 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
          $menuModel = Model::factory('menu_item');
          
          if(!$menuModel->updateLinks($object->getId(), $linksData))
-         	Response::jsonError($this->_lang->CANT_CREATE);
+         	$this->response->error($this->lang->get('CANT_CREATE'));
          	       
-         Response::jsonSuccess(array('id'=>$recId,));    
+         $this->response->success(array('id'=>$recId,));    
     }
     
     /**
      * (non-PHPdoc)
      * @see Backend_Controller_Crud::updateObject()
      */
-    public function updateObject(Orm\ObjectInterface $object)
+    public function updateObject(Orm\Object $object)
     {                            
         if(!$object->save())
-           Response::jsonError($this->_lang->CANT_EXEC); 
+           $this->response->error($this->lang->get('CANT_EXEC')); 
              	  
-        $linksData = Request::post('data', 'raw', false);
+        $linksData = $this->request->post('data', 'raw', false);
 
         if(strlen($linksData))
          	$linksData = json_decode($linksData , true);
@@ -150,16 +180,16 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
         $menuModel = Model::factory('Menu_Item');
         
         if(!$menuModel->updateLinks($object->getId(), $linksData))
-         	Response::jsonError($this->_lang->CANT_CREATE);   
+         	$this->response->error($this->lang->get('CANT_CREATE'));   
                  
-        Response::jsonSuccess(array('id'=>$object->getId()));          
+        $this->response->success(array('id'=>$object->getId()));          
     }
     /**
      * Import Site structure
      */
     public function importAction()
     {
-    	Response::jsonSuccess(Model::factory('menu_item')->exportsiteStructure());
+    	$this->response->success(Model::factory('menu_item')->exportsiteStructure());
     }
 
 
@@ -173,8 +203,8 @@ class Backend_Menu_Controller extends Backend_Controller_Crud
         /*
          * Module bootstrap
          */
-        if(file_exists($this->_configMain->get('jsPath').'app/system/desktop/' . strtolower($this->_module) . '.js'))
-            $projectData['includes']['js'][] = '/js/app/system/desktop/' . strtolower($this->_module) .'.js';
+        if(file_exists($this->appConfig->get('jsPath').'app/system/desktop/' . strtolower($this->getModule()) . '.js'))
+            $projectData['includes']['js'][] = '/js/app/system/desktop/' . strtolower($this->getModule()) .'.js';
 
         return $projectData;
     }
