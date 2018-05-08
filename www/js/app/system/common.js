578bd2fb35e24f8ba0fd5acb8ca5e8aa2a6fd4f6
hartois <hartois@gmail.com>
2018-05-08 12:09:24 +0300

    FIX: EXTJS-22672

--- www/js/app/system/common.js
+++ www/js/app/system/common.js
@@ -777,6 +777,56 @@ Ext.Ajax.on('beforerequest', function(connection, options) {
 		}
 	});
 })();
+
+Ext.define('Ext.Fix.EXTJS-22672', {
+    override: 'Ext.view.Table',
+
+    getDefaultFocusPosition: function(fromComponent) {
+        var me = this,
+            store = me.dataSource,
+            focusPosition = me.lastFocused,
+            newPosition = new Ext.grid.CellContext(me).setPosition(0, 0),
+            targetCell, scroller;
+        if (fromComponent) {
+            // Tabbing in from one of our column headers; the user will expect to land in that column.
+            // Unless it is configured cellFocusable: false
+            if (fromComponent.isColumn && fromComponent.cellFocusable !== false) {
+                if (!focusPosition) {
+                    focusPosition = newPosition;
+                }
+                focusPosition.setColumn(fromComponent);
+                focusPosition.setView(fromComponent.getView());
+            }
+            // Tabbing in from the neighbouring TableView (eg, locking).
+            // Go to column zero, same record
+            else if (fromComponent.isTableView && fromComponent.lastFocused && fromComponent.ownerGrid === me.ownerGrid) {
+                focusPosition = new Ext.grid.CellContext(me).setPosition(fromComponent.lastFocused.record, 0);
+            }
+        }
+        // We found a position from the "fromComponent, or there was a previously focused context
+        if (focusPosition) {
+            scroller = me.getScrollable();
+            // Record is not in the store, or not in the rendered block.
+            // Fall back to using the same row index.
+            if (!store.contains(focusPosition.record) || (scroller && !scroller.isInView(focusPosition.getRow()).y)) {
+                focusPosition.setRow(store.getAt(Math.min(focusPosition.rowIdx, store.getCount() - 1)));
+            }
+        } else // All else failes, find the first focusable cell.
+        {
+            focusPosition = newPosition;
+            // Find the first focusable cell.
+            targetCell = me.el.down(me.getCellSelector() + '[tabIndex="-1"]');
+            if (targetCell) {
+                focusPosition.setPosition(me.getRecord(targetCell), me.getHeaderByCell(targetCell));
+            } else // All visible columns are cellFocusable: false
+            {
+                focusPosition = null;
+            }
+        }
+        return focusPosition;
+    }
+});
+
 /*
  * Tooltips cut fix fo FF/Mac 4.2.x
  *
