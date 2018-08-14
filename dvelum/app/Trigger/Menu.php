f5997d6af474ac15c9e88d82bac9d7d3978b4732
Kirill <kirill.a.egorov@gmail.com>
2018-08-14 16:14:09 +0300

    Fox for Menu_Trigger

--- dvelum/app/Trigger/Menu.php
+++ dvelum/app/Trigger/Menu.php
@@ -6,32 +6,35 @@ use Dvelum\Service;
 
 class Trigger_Menu extends Trigger
 {
-	public function onAfterDelete(Orm\RecordInterface $object)
-	{		
-		if(!$this->_cache)
-			return;
-			
-		parent::onAfterDelete($object);			
-		$this->clearBlockCache($object);
-	}
-	
-	public function onAfterUpdate(Orm\RecordInterface $object)
-	{
-		if(!$this->_cache)
-			return;
-		
-		parent::onAfterUpdate($object);	
-		$this->clearBlockCache($object);	
-	}
+    public function onAfterDelete(Orm\RecordInterface $object)
+    {
+        if(!$this->_cache)
+            return;
 
-	public function clearBlockCache(Orm\RecordInterface $object)
-	{
-		if(!$this->_cache)
-			return;
-			
-		$menuModel = Model::factory('Menu');
-		$this->_cache->remove($menuModel->resetCachedMenuLinks($object->getId()));			
-		$blockManager = Service::get('blockManager');
-		$blockManager->invalidateCacheBlockMenu($object->getId());
-	}
+        parent::onAfterDelete($object);
+        $this->clearBlockCache($object);
+    }
+
+    public function onAfterUpdate(Orm\RecordInterface $object)
+    {
+        if(!$this->_cache)
+            return;
+
+        parent::onAfterUpdate($object);
+        $this->clearBlockCache($object);
+    }
+
+    public function clearBlockCache(Orm\RecordInterface $object)
+    {
+        if(!$this->_cache)
+            return;
+
+        /**
+         * @var Model_Menu $menuModel
+         */
+        $menuModel = Model::factory('Menu');
+        $menuModel->resetCachedMenuLinks($object->getId());
+        $blockManager = Service::get('blockManager');
+        $blockManager->invalidateCacheBlockMenu($object->getId());
+    }
 }
