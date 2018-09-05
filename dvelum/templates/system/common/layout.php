6a66c64da3669a85d8926958e429bc51057ed4e5
Kirill <kirill.a.egorov@gmail.com>
2018-09-05 08:43:01 +0300

    noindex for backoffice layout

--- dvelum/templates/system/common/layout.php
+++ dvelum/templates/system/common/layout.php
@@ -72,6 +72,7 @@ $wwwRoot = Request::wwwRoot();
 <head>
     <?php /*<BASE href="<?php echo Request::baseUrl();?>">*/?>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="robots" content="noindex">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <?php
