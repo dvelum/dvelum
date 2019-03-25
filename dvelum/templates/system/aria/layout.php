c200a903cb2a0d0d4fcc8a80c9a27ffe83b47f01
Kirill <kirill.a.egorov@gmail.com>
2019-03-25 11:57:15 +0300

    Bugfix. Cached csrf token

--- dvelum/templates/system/aria/layout.php
+++ dvelum/templates/system/aria/layout.php
@@ -1,4 +1,4 @@
 <?php
 if(!defined('DVELUM'))exit;
 
-echo $this->renderTemplate('system/common/layout.php', $this->getData());
+echo $this->renderTemplate('system/common/layout.php', $this->getData(), $this->useCache);
